3f5ae5524f90878739891b753142b960361dc199
Ayushman Bilas Thakur <ayushmanbilasthakur@gmail.com>
2025-01-27 17:08:58 +0530

    pong finished

--- codes/pong/ball.gd
+++ codes/pong/ball.gd
@@ -1,16 +1,19 @@
 extends RigidBody2D
 
-
-@onready var ball_pos = position
+var ball_pos = Vector2(500,280)
 const ball = preload("res://ball.tscn")
 
-func handle_ball_exit():
+func handle_ball_exit(i):
 	var new_ball = ball.instantiate()
-	get_tree().root.add_child(new_ball)
+	if(i == -1):
+		new_ball.linear_velocity = Vector2(-500,-350)
+	get_tree().root.get_child(0).add_child(new_ball)
 	new_ball.position = ball_pos
+	
 	queue_free()
 
-
-func _on_body_entered(body):
-	if body.is_in_group("trigger"):
-		handle_ball_exit()
+func _process(_delta):
+	if(global_position.x < -20):
+		handle_ball_exit(-1)
+	if(global_position.x > 980):
+		handle_ball_exit(1)
---
+++ codes/pong/world.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+@onready var leftScoreContainer = $Control/LeftScore
+@onready var rightScoreContainer = $Control/RightScore
+@onready var pausePanel = $Control/PausePanel
+
+var leftScore = -1
+var rightScore = 0
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		for child in get_children():
+			
+			child.get_tree().paused = !child.get_tree().paused
+			if child.get_tree().paused:
+				pausePanel.visible = true
+			else:
+				pausePanel.visible = false
+				
+
+func _on_child_entered_tree(node: Node) -> void:
+	if(node.is_in_group("ball")):
+		print_debug(node.linear_velocity.x)
+		if(node.linear_velocity.x > 0):
+			leftScore += 1
+		else:
+			rightScore += 1
+		if(leftScoreContainer && rightScoreContainer):
+			leftScoreContainer.clear()
+			rightScoreContainer.clear()
+			leftScoreContainer.add_text(str(leftScore))
+			rightScoreContainer.add_text(str(rightScore))
+		
